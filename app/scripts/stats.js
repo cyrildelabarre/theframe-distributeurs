2c9ede80cf9a021540a13f6f5cd1237b14f10be1
Delabarre Cyril <cyril.delabarre@hotmail.com>
2017-07-04 15:22:31 +0200

    cookie accept

---
+++ app/scripts/stats.js
@@ -0,0 +1,51 @@
+
+ (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-48481369-1', 'auto', {
+        'cookieExpires': 60 * 60 * 24 * 30 * 13 // Time in seconds.
+    }); ga('send', 'pageview');
+
+
+
+
+    s_pName = "fr:"+siteName+":"+pageName;
+    s_siteName=(typeof siteName != "undefined")?siteName:"";
+    s_siteType=(typeof siteType != "undefined")?siteType:"";
+    s_siteTitle=(typeof siteTitle != "undefined")?siteTitle:"";
+
+    /************* DO NOT ALTER ANYTHING BELOW THIS LINE ! **************/
+    //if(advjsTrackFlag){var s_adv_code=s_adv.t();if(s_adv_code)document.write(s_adv_code)};
+    var s_adv_code=s_adv.t();
+    if (s_adv_code) document.write(s_adv_code);
+  
+
+  
+                  /* You may give each page an identifying name, server, and channel on the  next lines. */
+                 s.pageName="fr:campaign:local:"+siteName+":"+pageName;
+                 s.hier1="fr>campaign>local>"+siteName+">"+pageName;
+                 s.channel="fr:campaign";
+                 s.prop1="fr";
+                 s.prop2="fr:campaign";
+                 s.prop3="fr:campaign:local" ;
+                 s.prop4="fr:campaign:local:"+siteName;
+                 s.prop5="fr:campaign:local:"+siteName+":"+pageName;
+
+                 /************* LOCALIZE THE RSID AND DO NOT ALTER ANYTHING BELOW THIS LINE ! **************/
+                  var s_code=s.t();if(s_code)document.write(s_code);
+   
+
+
+
+   
+
+   
+
+
+   
+  
+
+
+
+
+
+
+
